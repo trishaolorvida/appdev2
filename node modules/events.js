45880b42f1697d7d6af4d042974024af5b8fa84d
trisha.olorvida0207 <trisha.olorvida0207@gmail.com>
2025-03-08 04:30:41 +0000

    example events

--- node modules/events.js
+++ node modules/events.js
@@ -2,9 +2,23 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // Create a listener
-emitter.on('event', () => {
-  console.log('Event triggered!');
+emitter.on('Start', () => {
+  console.log('Starting application!');
 });
 
+emitter.on('event', (data), () => {
+    console.log('Name: $(data.name)');
+  });
+
+emitter.on('event', (data), () => {
+    console.log('Age: $(data.age)');
+  });
+
+emitter.on('Close', () => {
+    console.log('Application is closed!');
+  });
+
 // Trigger the event
-emitter.emit('event');
+emitter.emit ('Start');
+emitter.emit('event', { name: "Trisha", age: 20 });
+emitter.emit ('Close');
