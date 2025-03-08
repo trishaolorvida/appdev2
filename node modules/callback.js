4a7ceb189a3719f65ebe4fa2b34c7e073fbe4279
trisha.olorvida0207 <trisha.olorvida0207@gmail.com>
2025-03-08 04:52:51 +0000

    callback

---
+++ node modules/callback.js
@@ -0,0 +1,27 @@
+const name = "Trisha";
+const callback = function () {
+   console.log("This is a callback.");
+};
+
+function greet(name, callback) {
+    console.log("Hello, " + name);
+    callback();
+  }
+  
+//   greet('Alice', () => {
+//     console.log('Callback function executed!');
+//   });
+
+function greet(name) {
+    console.log("Hello, " + name);
+    
+    const callback = function () {
+        console.log("This is another function inside a function.");
+     };
+     callback();
+  }
+
+  greet(name);
+
+// greet(name, callback);
+  
